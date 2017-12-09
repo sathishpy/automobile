2372882265eb9c465c5fcaafaf5dfed08562e68e
Sathisha Poojary <sathishpy@gmail.com>
2017-12-09 15:25:36 +0530

    Customize Sales Invoice to include vehicle details. Generate invoice from job card

--- automobile/automobile/doctype/auto_service_job_card/auto_service_job_card.js
+++ automobile/automobile/doctype/auto_service_job_card/auto_service_job_card.js
@@ -3,6 +3,39 @@
 
 frappe.ui.form.on('Auto Service Job Card', {
 	refresh: function(frm) {
+		if (frm.doc.docstatus == 1) {
+			frm.add_custom_button(__('Generate Invoice'), function() {
+					frm.events.make_sales_invoice(frm)
+			});
+		}
 
-	}
+	},
+	invoke_function(frm, method) {
+		frappe.call({
+			doc: frm.doc,
+			method: method,
+			callback: function(r) {
+				if(!r.exe) {
+					frm.refresh_fields()
+				}
+			}
+		});
+	},
+
+	vehicle: function(frm) {
+		if (frm.doc.docstatus != 1) {
+			frm.set_value("service_date", frappe.datetime.nowdate())
+		}
+		frm.add_fetch("vehicle", "chassis_number", "chassis_number")
+		frm.add_fetch("vehicle", "vehicle_model", "vehicle_model")
+		frm.add_fetch("vehicle", "odometer", "odometer")
+		frm.add_fetch("vehicle", "owner", "customer")
+		frm.add_fetch("vehicle", "service_center", "service_center")
+	},
+	make_sales_invoice: function(frm) {
+		frappe.model.open_mapped_doc({
+			method: "automobile.automobile.doctype.auto_service_job_card.auto_service_job_card.make_sales_invoice",
+			frm: frm,
+		})
+	},
 });
--- automobile/automobile/doctype/auto_service_job_card/auto_service_job_card.py
+++ automobile/automobile/doctype/auto_service_job_card/auto_service_job_card.py
@@ -4,8 +4,36 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class AutoServiceJobCard(Document):
-	def autoname(self):
-		self.name = self.vehicle + "-" + self.service_date
+	def before_submit(self):
+		if (self.start_time is None):
+			frappe.throw(_("Jobcard starting time is missing"))
+		if (self.end_time is None):
+			frappe.throw(_("Jobcard finishing time is missing"))
+		if (self.job_items is None or len(self.job_items) == 0):
+			frappe.throw(_("No job items added to job card"))
+
+@frappe.whitelist()
+def make_sales_invoice(jobcard):
+	jobdoc = frappe.get_doc("Auto Service Job Card", jobcard)
+	si = frappe.new_doc("Sales Invoice")
+	si.vehicle = jobdoc.vehicle
+	si.odometer = jobdoc.odometer
+	si.chassis_number = jobdoc.chassis_number
+	si.service_center = jobdoc.service_center
+	si.job_id = jobcard
+	si.technician = jobdoc.technician
+	si.customer = jobdoc.customer
+	#disposal_account, depreciation_cost_center = get_disposal_account_and_cost_center(company)
+	for jobitem in jobdoc.job_items:
+		si.append("items", 	{
+			"item_code": jobitem.item,
+			"qty": jobitem.qty,
+			"stock_qty": jobitem.qty,
+		})
+	si.set_missing_values()
+	si.due_date = si.posting_date
+	return si
